49a8493029bbbb9ed99c8da5fe169fd6c5f6369e
Srikanth Nani <55689131+iamsrikanthnani@users.noreply.github.com>
2023-03-05 15:43:22 +0530

    Update BaseUserMessage.tsx

--- packages/uikit-react-native/src/components/MessageRenderer/UserMessage/BaseUserMessage.tsx
+++ packages/uikit-react-native/src/components/MessageRenderer/UserMessage/BaseUserMessage.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-import { RegexText, Text, createStyleSheet, useUIKitTheme } from '@sendbird/uikit-react-native-foundation';
-import { urlRegexStrict } from '@sendbird/uikit-utils';
+import {
+  RegexText,
+  Text,
+  createStyleSheet,
+  useUIKitTheme,
+} from "@sendbird/uikit-react-native-foundation";
+import { urlRegexStrict } from "@sendbird/uikit-utils";
 
-import { useLocalization, useSendbirdChat, useUserProfile } from '../../../hooks/useContext';
-import SBUUtils from '../../../libs/SBUUtils';
-import type { UserMessageProps } from './index';
-
+import {
+  useLocalization,
+  useSendbirdChat,
+  useUserProfile,
+} from "../../../hooks/useContext";
+import SBUUtils from "../../../libs/SBUUtils";
+import type { UserMessageProps } from "./index";
+import LexicalJSONParser from "global-components/LexicalJSONParser";
+ 
 const BaseUserMessage = ({
   message,
   variant,
@@ -21,62 +31,76 @@ const BaseUserMessage = ({
   const { STRINGS } = useLocalization();
   const { colors, palette } = useUIKitTheme();
 
-  const color = colors.ui.groupChannelMessage[variant][pressed ? 'pressed' : 'enabled'];
+  const color = colors.ui.message[variant][pressed ? "pressed" : "enabled"];
 
   return (
     <View style={[styles.container, { backgroundColor: color.background }]}>
       <View style={styles.wrapper}>
         <Text body3 color={color.textMsg}>
-          <RegexText
-            body3
-            color={color.textMsg}
-            patterns={[
-              {
-                regex: mentionManager.templateRegex,
-                replacer({ match, groups, parentProps, index, keyPrefix }) {
-                  const user = message.mentionedUsers?.find((it) => it.userId === groups[2]);
-                  if (user) {
+          {message?.data ? (
+            <LexicalJSONParser
+              json={JSON.parse(message?.data)}
+              textColor={color.textMsg}
+            />
+          ) : (
+            <RegexText
+              body3
+              color={color.textMsg}
+              patterns={[
+                {
+                  regex: mentionManager.templateRegex,
+                  replacer({ match, groups, parentProps, index, keyPrefix }) {
+                    const user = message.mentionedUsers?.find(
+                      (it) => it.userId === groups[2]
+                    );
+                    if (user) {
+                      return (
+                        <Text
+                          {...parentProps}
+                          key={`${keyPrefix}-${index}`}
+                          onPress={() => show(user)}
+                          onLongPress={onLongPressMentionedUser}
+                          style={[
+                            parentProps?.style,
+                            { fontWeight: "bold" },
+                            user.userId === currentUser?.userId && {
+                              backgroundColor: palette.highlight,
+                            },
+                          ]}
+                        >
+                          {`${mentionManager.asMentionedMessageText(user)}`}
+                        </Text>
+                      );
+                    }
+                    return match;
+                  },
+                },
+                {
+                  regex: urlRegexStrict,
+                  replacer({ match, parentProps, index, keyPrefix }) {
                     return (
                       <Text
                         {...parentProps}
                         key={`${keyPrefix}-${index}`}
-                        onPress={() => show(user)}
-                        onLongPress={onLongPressMentionedUser}
+                        onPress={() => SBUUtils.openURL(match)}
+                        onLongPress={onLongPressURL}
                         style={[
                           parentProps?.style,
-                          { fontWeight: 'bold' },
-                          user.userId === currentUser?.userId && { backgroundColor: palette.highlight },
+                          { textDecorationLine: "underline" },
                         ]}
                       >
-                        {`${mentionManager.asMentionedMessageText(user)}`}
+                        {match}
                       </Text>
                     );
-                  }
-                  return match;
+                  },
                 },
-              },
-              {
-                regex: urlRegexStrict,
-                replacer({ match, parentProps, index, keyPrefix }) {
-                  return (
-                    <Text
-                      {...parentProps}
-                      key={`${keyPrefix}-${index}`}
-                      onPress={() => SBUUtils.openURL(match)}
-                      onLongPress={onLongPressURL}
-                      style={[parentProps?.style, { textDecorationLine: 'underline' }]}
-                    >
-                      {match}
-                    </Text>
-                  );
-                },
-              },
-            ]}
-          >
-            {mentionManager.shouldUseMentionedMessageTemplate(message)
-              ? message.mentionedMessageTemplate
-              : message.message}
-          </RegexText>
+              ]}
+            >
+              {mentionManager.shouldUseMentionedMessageTemplate(message)
+                ? message.mentionedMessageTemplate
+                : message.message}
+            </RegexText>
+          )}
           {Boolean(message.updatedAt) && (
             <Text body3 color={color.textEdited}>
               {STRINGS.GROUP_CHANNEL.MESSAGE_BUBBLE_EDITED_POSTFIX}
@@ -91,7 +115,7 @@ const BaseUserMessage = ({
 const styles = createStyleSheet({
   container: {
     borderRadius: 16,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   wrapper: {
     paddingHorizontal: 12,
